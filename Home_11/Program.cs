16cacbe9607f03203ffdec175c26a4343293671b
Микола Селецький <Seletskyi.Mykola@outlook.com>
2020-11-02 02:35:46 +0200

    task_1 task_2

--- Home_11/Program.cs
+++ Home_11/Program.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 using System.Threading;
 namespace Home_11
 {
-    static class Task_1 
+    static class Task_1
     {
         class AlarmEventArgs
         {
             public UInt16 alarmHour { get; }
             public UInt16 alarmMinute { get; }
             public string message { get; }
-        public AlarmEventArgs(UInt16 alarmHour = 0, UInt16 alarmMinute = 0,string message="") :base()
+            public AlarmEventArgs(UInt16 alarmHour = 0, UInt16 alarmMinute = 0, string message = "") : base()
             {
                 this.alarmHour = alarmHour;
                 this.alarmMinute = alarmMinute;
@@ -37,24 +37,24 @@ namespace Home_11
             static public void Tick(object obj, AlarmEventArgs alarm)
             {
                 Watch tmp = obj as Watch;
-                if (tmp!=null)
+                if (tmp != null)
                 {
                     tmp.minute = tmp.minute + 1 == 60 ? (UInt16)0 : (UInt16)(tmp.minute + 1);
-                    tmp.hour += tmp.minute == 0 ? (tmp.hour+1==24?(UInt16)0 : (UInt16)1): (UInt16)0;
-                Console.SetCursorPosition(0, 0);
-                Console.Write($"{tmp.hour:00}:{tmp.minute:00}");
+                    tmp.hour += tmp.minute == 0 ? (tmp.hour + 1 == 24 ? (UInt16)0 : (UInt16)1) : (UInt16)0;
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write($"{tmp.hour:00}:{tmp.minute:00}");
 
                 }
-                if (alarm?.alarmHour== tmp.hour &&alarm?.alarmMinute== tmp.minute)
+                if (alarm?.alarmHour == tmp.hour && alarm?.alarmMinute == tmp.minute)
                 {
                     tmp.alarmStart(alarm);
                 }
             }
 
         }
-public static void main()
+        public static void main()
         {
-            Watch w = new Watch(13,49);
+            Watch w = new Watch(13, 49);
             AlarmEventArgs alarm = new AlarmEventArgs(14, 1, "Good morning");
             for (int i = 0; i < 60; i++)
             {
@@ -63,11 +63,118 @@ public static void main()
             }
         }
     }
+    class Task_2
+    {
+        class Car
+        {
+            static public UInt16 counterID = 0;
+            static Random rnd = new Random();
+            static public UInt16 MaxId => counterID;
+
+            public delegate void FinishCar(Car car);
+            const UInt16 MinSpeed = 1;
+            const UInt16 MaxSpeed = 5;
+            UInt16 speed = (UInt16)rnd.Next(MinSpeed, MaxSpeed);
+            UInt16 id = counterID++;
+            UInt16 position = 0;
+            event FinishCar finish;
+            public FinishCar Finish
+            {
+                set => finish = value;
+            }
+            void newSpeed()
+            {
+                if (rnd.Next(0, 1) == 0)
+                {
+                    speed = (UInt16)rnd.Next(MinSpeed, MaxSpeed);
+                }
+            }
+            public void Go()
+            {
+                newSpeed();
+                position = (position + speed > 100) ? (UInt16)100 : (UInt16)(position + (UInt16)speed);
+                Console.SetCursorPosition(position, id);
+                Console.WriteLine(this.GetType().Name);
+                if (position == 100)
+                {
+                    finish(this);
+                }
+            }
+            public void ToStart()
+            {
+                position = 0;
+                Console.SetCursorPosition(position, id);
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
+        class SportsCar : Car
+        {
+
+        }
+        class TrucksCar : Car
+        {
+
+        }
+        class BusesCar : Car
+        {
+
+        }
+        class Game
+        {
+
+            List<Car> cars = new List<Car>();
+            Action toStart;
+            Action carsGo;
+            bool finish = false;
+            public Game(List<Car> cars)
+            {
+                this.cars = cars;
+                for (int i = 0; i < cars.Count; i++)
+                {
+                    toStart += cars[i].ToStart;
+                    cars[i].Finish = Finish;
+                    carsGo += cars[i].Go;
+
+                }
+            }
+            void Finish(Car car)
+            {
+                finish = true;
+                Console.SetCursorPosition(0, Car.MaxId);
+                Console.WriteLine("Won " + car.GetType().Name);
+                Thread.Sleep(2000);
+            }
+            public void ToStart()
+            {
+                Console.Clear();
+                finish = false;
+                toStart();
+            }
+            public void Go()
+            {
+
+                while (!finish)
+                {
+                    Console.Clear();
+                    carsGo();
+                    Thread.Sleep(111);
+                }
+            }
+        }
+
+        static public void main()
+        {
+            Game a = new Game(new List<Car>() { new Car(), new SportsCar(), new TrucksCar(), new BusesCar() });
+            a.Go();
+            a.ToStart();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Task_1.main();
+             //Task_1.main();
+             Task_2.main();
         }
     }
 }
